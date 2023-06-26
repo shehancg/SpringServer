33fa9af7e59aef2efb5468ab23a1695efc234b37
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-06-26 23:35:02 +0530

    Added service interface

---
+++ src/main/java/com/example/demoserver/service/ServerService.java
@@ -0,0 +1,13 @@
+package com.example.demoserver.service;
+
+import org.apache.catalina.Server;
+
+import java.util.Collection;
+
+public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
