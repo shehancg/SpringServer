69f094974dff0c216ce0d34f900e0e65da1dc75d
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-06-27 19:13:50 +0530

    Added remaining service implementations

--- src/main/java/com/example/demoserver/service/impl/ServerServiceImpl.java
+++ src/main/java/com/example/demoserver/service/impl/ServerServiceImpl.java
@@ -9,11 +9,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.Random;
+
+import static java.lang.Boolean.TRUE;
+import static org.springframework.data.domain.PageRequest.of;
 
 @RequiredArgsConstructor
 @Service
@@ -40,23 +44,31 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0,limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by ID:{}",id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating Server: {}",server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting Server by ID:{}",id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
-    private String setServerImageUrl() { return null; }
+    private String setServerImageUrl() {
+        String[] imageNames = {"server1.png","server2.png","server3.png","server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(4)]).toUriString();
+    }
 }
